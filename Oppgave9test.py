68b24afae919b58502ee9c145307606f76142285
OleBSylte <116606180+OleBSylte@users.noreply.github.com>
2022-11-02 16:19:12 +0100

    Update Oppgave9test.py

--- Oppgave9test.py
+++ Oppgave9test.py
@@ -2,6 +2,7 @@ from datetime import datetime
 import csv
 
 
+# OPPGAVE D & E
 class Avtale:
     def __init__(self, tittel, sted, tidspunkt, varighet):
         self.tittel = tittel  # En streng
@@ -10,35 +11,39 @@ class Avtale:
         self.varighet = varighet  # Et integer
 
     def __str__(self):
-        return f"Avtalen er: {self.tittel};Sted: {self.sted}.;Tidspunkt: {self.tidspunkt}.;Varighet: {self.varighet}"
+        return f"Avtalen er: {self.tittel}; Sted: {self.sted}; Tidspunkt: {self.tidspunkt}; skoVarighet: {self.varighet}"
 
 
+# OPPGAVE F
 def avtale(liste):
-    tittel = str('skole')
+    tittel = str(input("Skriv inn tittel: "))
     sted = str('Skolen')
-    dato = '2022-11-04 12:00:00'
+    dato = input("Skriv inn dato: ")
     tidspunkt = datetime.fromisoformat(dato)
     varighet = int(120)
     avtalen = Avtale(tittel, sted, tidspunkt, varighet)
     liste.append(avtalen)
+    return tidspunkt, tittel
+# 2022-11-04 12:00:00
+# 2022-11-05 12:00:01
 
 
-#2022-11-04 12:00:00
-
-
+# Skriver ut avtalene til brukeren # OPPGAVE G
 def skriv_ut_avtaler(lister):
     print("########## AVTALER ##########")
     for i in lister:
         print(i)
 
 
-####### USIKKER PÅ DENNE ##########
+# lager en txt fil med avtalene som er oppgitt # OPPGAVE H
 def lagrer_liste_med_avtaler(lister):  # Skal lagre listen med avtaler som ei txt.fil
     with open("liste_med_avtaler.txt", "w", encoding="UTF8") as lmf:
         for i in lister:
             lmf.write(str(i))
+            lmf.write("\n")
 
 
+# henter avtalene fra en fil # OPPGAVE I
 def henter_avtale_fra_fil(filnavn):
     with open(filnavn, "r", encoding="UTF8") as fila:
         liste_fra_fil = []
@@ -48,8 +53,24 @@ def henter_avtale_fra_fil(filnavn):
     return liste_fra_fil
 
 
+# finner avtaler med samme dato # OPPGAVE J
+def samme_dato(liste, dato):
+    for e in range(len(liste)):
+        if dato in str(liste[e]):
+            print(liste[e])
+
+
+# Finner samme tittel # OPPGAVE K
+def samme_tittel(liste, tittel):
+    for e in range(len(liste)):
+        if tittel in str(liste[e]):
+            print(liste[e])
+
+
 liste_med_avtaler = []
+avtale(liste_med_avtaler)
 avtale(liste_med_avtaler)
 skriv_ut_avtaler(liste_med_avtaler)
 lagrer_liste_med_avtaler(liste_med_avtaler)
 henter_avtale_fra_fil("liste_med_avtaler.txt")
+samme_dato(liste_med_avtaler, '2022-11-04')
