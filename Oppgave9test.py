b7eecac8325c5b8e0b922b3b3bb89cc282bb7b34
OleBSylte <116606180+OleBSylte@users.noreply.github.com>
2022-11-02 15:25:53 +0100

    Update Oppgave9test.py

--- Oppgave9test.py
+++ Oppgave9test.py
@@ -10,18 +10,17 @@ class Avtale:
         self.varighet = varighet  # Et integer
 
     def __str__(self):
-        return f"Avtalen er: {self.tittel}.\nSted: {self.sted}.\nTidspunkt: {self.tidspunkt}.\nVarighet: {self.varighet}"
+        return f"Avtalen er: {self.tittel};Sted: {self.sted}.;Tidspunkt: {self.tidspunkt}.;Varighet: {self.varighet}"
 
 
-def avtale():
+def avtale(liste):
     tittel = str('skole')
     sted = str('Skolen')
     dato = '2022-11-04 12:00:00'
     tidspunkt = datetime.fromisoformat(dato)
     varighet = int(120)
-    avtale = Avtale(tittel, sted, tidspunkt, varighet)
-    liste_med_avtaler.append(avtale)
-    return avtale
+    avtalen = Avtale(tittel, sted, tidspunkt, varighet)
+    liste.append(avtalen)
 
 
 #2022-11-04 12:00:00
@@ -32,6 +31,8 @@ def skriv_ut_avtaler(lister):
     for i in lister:
         print(i)
 
+
+####### USIKKER PÅ DENNE ##########
 def lagrer_liste_med_avtaler(lister):  # Skal lagre listen med avtaler som ei txt.fil
     with open("liste_med_avtaler.txt", "w", encoding="UTF8") as lmf:
         for i in lister:
@@ -41,13 +42,14 @@ def lagrer_liste_med_avtaler(lister):  # Skal lagre listen med avtaler som ei tx
 def henter_avtale_fra_fil(filnavn):
     with open(filnavn, "r", encoding="UTF8") as fila:
         liste_fra_fil = []
-        for e in csv.reader(fila, delimiter="\n"):
+        for e in csv.reader(fila, delimiter=";"):
             liste_fra_fil.append(e)
     print(liste_fra_fil)
+    return liste_fra_fil
 
 
 liste_med_avtaler = []
-avtale_1 = avtale()
+avtale(liste_med_avtaler)
 skriv_ut_avtaler(liste_med_avtaler)
 lagrer_liste_med_avtaler(liste_med_avtaler)
 henter_avtale_fra_fil("liste_med_avtaler.txt")
