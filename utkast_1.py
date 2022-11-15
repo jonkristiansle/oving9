7ac8b84d1751a7213f58de6d63e496b03fcc2bf3
OleBSylte <116606180+OleBSylte@users.noreply.github.com>
2022-11-15 11:43:54 +0100

    Add files via upload

---
+++ utkast_1.py
@@ -0,0 +1,143 @@
+"""
+@authors: Ole Berg Sylte, Stian Kolnes, Edward Larsen og Jon Slettebø
+"""
+from datetime import datetime
+import csv
+
+
+class Avtale:
+    def __init__(self, tittel, sted, tidspunkt, varighet):
+        self.tittel = tittel  # En streng
+        self.sted = sted  # En streng
+        self.tidspunkt = tidspunkt  # Et objekt
+        self.varighet = varighet  # Et integer
+
+    def __str__(self):
+        return f"Avtalen er: {self.tittel}; Sted: {self.sted}; Tidspunkt: {self.tidspunkt}; Varighet: {self.varighet}"
+
+
+def avtale():
+    tittel = str(input("Hva er avtale emne? "))
+    sted = str(input("Hvor skal avtalen skje? "))
+    dato = input("Når er avtalen? Skriv på denne måten: ÅÅÅÅ-MM-DD TT:MM:SS ")
+    tidspunkt = datetime.fromisoformat(dato)
+    varighet = int(input("Hvor mange minutter skal avtalen vare? "))
+    avtalen = Avtale(tittel, sted, tidspunkt, varighet)
+    return avtalen
+
+
+def skriv_ut_avtaler(lister):
+    print("########## AVTALER ##########")
+    for i in lister:
+        print(i)
+
+
+def lagrer_liste_med_avtaler(lister):  # Skal lagre listen med avtaler som ei txt.fil
+    with open("liste_med_avtaler.txt", "w", encoding="UTF8") as lmf:
+        for i in lister:
+            lmf.write(str(i))
+            lmf.write('\n')
+
+
+def henter_avtale_fra_fil(filnavn):
+    with open(filnavn, "r", encoding="UTF8") as fila:
+        liste_fra_fil = []
+        for e in csv.reader(fila, delimiter=";"):
+            liste_fra_fil.append(e)
+    print(liste_fra_fil)
+    return liste_fra_fil
+
+
+# finner avtaler med samme dato # OPPGAVE J
+def samme_dato(liste, dato):
+    for e in range(len(liste)):
+        if dato in str(liste[e]):
+            print(liste[e])
+
+
+# Finner samme tittel # OPPGAVE K
+def samme_tittel(liste, tittel):
+    for e in range(len(liste)):
+        if tittel in str(liste[e]):
+            print(liste[e])
+
+
+liste_med_avtaler = []
+
+
+def meny():
+    operation = input('''\
+    Select operation:
+----------------------------
+[1] Lese inn avtalve fra fil
+[2] Skriv avtalene til fil
+[3] Ny avtale
+[4] Skriv ut alle avtalene
+[5] Avslutt
+[6] Slett avtale 
+[7] Redigere avtale 
+Skriv inn her: ''')
+    if operation == '1':
+        henter_avtale_fra_fil('liste_med_avtaler.txt')
+        igjen()
+    elif operation == '2':
+        lagrer_liste_med_avtaler(liste_med_avtaler)
+        igjen()
+    elif operation == '3':
+        liste_med_avtaler.append(avtale())
+        igjen()
+    elif operation == '4':
+        skriv_ut_avtaler(liste_med_avtaler)
+        igjen()
+    elif operation == '5':
+        print('Ok, hadde :)')
+    elif operation == '6':
+        skriv_ut_avtaler(liste_med_avtaler)
+        del liste_med_avtaler[int(input('Hvilken avtale vil du slette: '))-1]
+        igjen()
+    elif operation == '7':
+        skriv_ut_avtaler(liste_med_avtaler)
+        indeks = int(input("redigere: ")) - 1
+        liste_med_avtaler[indeks] = avtale()
+        igjen()
+    else:
+        print('Du må velge et gyldig tall.')
+        meny()
+
+
+def igjen():
+    list_again = input('''
+Har du lyst til å gjøre flere endringer (J/N)
+''')
+
+    if list_again.upper() == 'J':
+        meny()
+    elif list_again.upper() == 'N':
+        print('Ok, hadde :)')
+    else:
+        igjen()
+
+
+# 2022-11-04 12:00:00
+
+
+class Kategori:
+    def __init__(self, id_n, navn, prioritet=1):
+        self.id_n = id_n
+        self.navn = navn
+        self.prioritet = prioritet
+
+    def __str__(self):
+        return f"Avtalens id: {self.id_n}, avtalens navn: {self.navn}, avtalens prioritet: {self.prioritet}"
+
+
+def leserinn():
+    id_n = input("Skriv inn avtalens id: ")
+    navn = input("Skriv inn avtalens navn: ")
+    prioritet = int(input("Skriv inn avtalens prioritet: "))
+    return Kategori(id_n, navn, prioritet)
+
+
+x = leserinn()
+print(x)
+print(type(x))
